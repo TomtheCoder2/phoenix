f8b321bdd683ffa59b30dac93c21504c135c8311
TomtheCoder2 <jan.wilhem77@gmail.com>
2023-08-09 19:20:11 +0200

    bin fix

--- src/bin/lang_test.rs
+++ src/bin/lang_test.rs
@@ -2,7 +2,7 @@ use phoenix::compiler::Compiler;
 
 fn main() {
     let code = "print 1 + 11;".to_string();
-    let mut compiler = Compiler::new_file("script".to_string(), code, false);
+    let mut compiler = Compiler::new_file("script".to_string(), code, false, 0);
     let res = compiler.compile(false).unwrap();
     // print the code
     println!("{:?}", res);
