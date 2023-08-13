a3f62623b4b72bff5c82a1670a38fa27020a6335
TomtheCoder2 <jan.wilhem77@gmail.com>
2023-08-13 23:05:25 +0200

    error message

--- src/log.rs
+++ src/log.rs
@@ -18,6 +18,7 @@ macro_rules! warn {
 macro_rules! error {
     ($($arg:tt)*) => {{
         use colored::Colorize;
+        // reference to: https://xkcd.com/2200/
         eprintln!("{} {}", "[ERROR]".red(), format!($($arg)*));
     }};
 }
@@ -36,7 +37,14 @@ macro_rules! phoenix_error {
     ($($arg:tt)*) => {{
         use colored::Colorize;
         use std::process::exit;
-        eprintln!("{} {}\n\tPlease Report this as a bug", "[PHOENIX ERROR]".red(), format!($($arg)*));
+        // reference to: https://xkcd.com/2200/
+        eprintln!("[{}:{}] {} {}\nPlease Report this as a bug here: https://github.com/TomtheCoder2/phoenix\n\n{}", file!(), line!(), "[PHOENIX ERROR]".red(), format!($($arg)*), "\
+        If you're seeing this, the code is in what I thought was an unreachable state. \
+        I could give you advice for what to do. \
+        But honestly, why should you trust me? \
+        I clearly screwed this up. I'm writing a message that should never appear, yet \
+        I know it will probably appear someday. \
+        On a deep level, I know I'm not up to this task. I'm so sorry.");
         exit(1);
     }};
 }
